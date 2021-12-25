dc2208bb42d7287e26fe80e5e25d9b3d962d4354
ronimizy <71693183+ronimizy@users.noreply.github.com>
2021-12-26 00:04:18 +0300

    chore: ef entities setup

---
+++ Bebruber.DataAccess/BebruberDatabaseContext.cs
@@ -0,0 +1,35 @@
+using Bebruber.Domain.Entities;
+using Bebruber.Domain.ValueObjects;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bebruber.DataAccess
+{
+    public class BebruberDatabaseContext : DbContext
+    {
+        public BebruberDatabaseContext(DbContextOptions options)
+            : base(options) { }
+
+        public DbSet<Client> Clients { get; private set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            ConfigureClient(modelBuilder);
+            ConfigurePaymentInfo(modelBuilder);
+        }
+
+        private static void ConfigureClient(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Client>().OwnsOne(c => c.Name);
+            modelBuilder.Entity<Client>().OwnsOne(nameof(Rating), c => c.Rating);
+            modelBuilder.Entity<Client>().OwnsOne(c => c.PaymentAddress);
+            modelBuilder.Entity<Client>().Navigation(c => c.PaymentInfos).HasField("_paymentInfos");
+        }
+
+        private static void ConfigurePaymentInfo(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PaymentInfo>().OwnsOne(nameof(CardNumber), i => i.CardNumber);
+            modelBuilder.Entity<PaymentInfo>().OwnsOne(nameof(CvvCode), i => i.CvvCode);
+            modelBuilder.Entity<PaymentInfo>().OwnsOne(i => i.ExpirationDate);
+        }
+    }
+}
