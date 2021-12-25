3f8f1186c5be257ed39018906038f48d8c457f69
ronimizy <71693183+ronimizy@users.noreply.github.com>
2021-12-26 00:03:33 +0300

    chore: domain tools

---
+++ Bebruber.Domain/Tools/BebruberException.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Bebruber.Domain.Tools
+{
+    public abstract class BebruberException : Exception
+    {
+        protected BebruberException() { }
+
+        protected BebruberException(string? message)
+            : base(message) { }
+
+        protected BebruberException(string? message, Exception? innerException)
+            : base(message, innerException) { }
+    }
+}
