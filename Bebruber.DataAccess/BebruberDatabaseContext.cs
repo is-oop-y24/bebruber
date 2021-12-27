ec647bf697685c4ec881b6e76c789792e83595a5
ronimizy <71693183+ronimizy@users.noreply.github.com>
2021-12-27 22:26:03 +0300

    refactor: database context

--- Bebruber.DataAccess/BebruberDatabaseContext.cs
+++ Bebruber.DataAccess/BebruberDatabaseContext.cs
@@ -1,5 +1,4 @@
 using Bebruber.Domain.Entities;
-using Bebruber.Domain.ValueObjects;
 using Microsoft.EntityFrameworkCore;
 
 namespace Bebruber.DataAccess;
@@ -10,26 +9,5 @@ public class BebruberDatabaseContext : DbContext
         : base(options) { }
 
     public DbSet<Client> Clients { get; private set; } = null!;
-    public DbSet<PaymentInfo> PaymentInfos { get; private set; } = null!;
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        ConfigureClient(modelBuilder);
-        ConfigurePaymentInfo(modelBuilder);
-    }
-
-    private static void ConfigureClient(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<Client>().OwnsOne(c => c.Name);
-        modelBuilder.Entity<Client>().OwnsOne(nameof(Rating), c => c.Rating);
-        modelBuilder.Entity<Client>().OwnsOne(c => c.PaymentAddress);
-        modelBuilder.Entity<Client>().Navigation(c => c.PaymentInfos).HasField("_paymentInfos");
-    }
-
-    private static void ConfigurePaymentInfo(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<PaymentInfo>().OwnsOne(nameof(CardNumber), i => i.CardNumber);
-        modelBuilder.Entity<PaymentInfo>().OwnsOne(nameof(CvvCode), i => i.CvvCode);
-        modelBuilder.Entity<PaymentInfo>().OwnsOne(i => i.ExpirationDate);
-    }
+    public DbSet<CardInfo> PaymentInfos { get; private set; } = null!;
 }
