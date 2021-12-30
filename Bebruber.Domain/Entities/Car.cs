0c869d981eea43668401cecda077ce6c1354efaa
ronimizy <71693183+ronimizy@users.noreply.github.com>
2021-12-30 23:39:37 +0300

    feat: car

---
+++ Bebruber.Domain/Entities/Car.cs
@@ -0,0 +1,23 @@
+using Bebruber.Domain.Tools;
+using Bebruber.Domain.ValueObjects;
+using Bebruber.Utility.Extensions;
+
+namespace Bebruber.Domain.Entities;
+
+public class Car : Entity<Car>
+{
+    public Car(Driver owner, CarBrand brand, CarName name, CarColor color)
+    {
+        Owner = owner.ThrowIfNull();
+        Brand = brand.ThrowIfNull();
+        Name = name.ThrowIfNull();
+        Color = color.ThrowIfNull();
+    }
+
+    private Car() { }
+
+    public Driver Owner { get; protected init; }
+    public CarBrand Brand { get; private init; }
+    public CarName Name { get; private init; }
+    public CarColor Color { get; private init; }
+}
