c82d321f10dad37e94b511ade0a28a0416003a34
ronimizy <71693183+ronimizy@users.noreply.github.com>
2021-12-30 23:40:13 +0300

    feat: entity configurations

---
+++ Bebruber.DataAccess/Configurations/CarConfiguration.cs
@@ -0,0 +1,20 @@
+using System.Drawing;
+using Bebruber.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Bebruber.DataAccess.Configurations;
+
+public class CarConfiguration : IEntityTypeConfiguration<Car>
+{
+    public void Configure(EntityTypeBuilder<Car> builder)
+    {
+        builder.HasOne(c => c.Owner);
+        builder.OwnsOne(c => c.Brand);
+        builder.OwnsOne(c => c.Name);
+        builder.OwnsOne(c => c.Color)
+            .Property(c => c.Color)
+            .HasConversion(c => c.ToArgb(),
+                i => Color.FromArgb(i));
+    }
+}
