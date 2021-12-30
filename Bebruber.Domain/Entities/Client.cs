6a416b9e4f6949fce3a25458d297437f12b2f7f3
ronimizy <71693183+ronimizy@users.noreply.github.com>
2021-12-30 23:38:51 +0300

    refactor: added null checks in Client

--- Bebruber.Domain/Entities/Client.cs
+++ Bebruber.Domain/Entities/Client.cs
@@ -10,23 +10,25 @@ public class Client : Entity<Client>
 {
     private readonly List<CardInfo> _paymentInfos;
 
-    public Client(Name name, Rating rating, Address? paymentAddress)
+    public Client(Name name, Rating rating, Address paymentAddress)
     {
+        _paymentInfos = new List<CardInfo>();
         Name = name.ThrowIfNull();
         Rating = rating.ThrowIfNull();
-        PaymentAddress = paymentAddress;
-        _paymentInfos = new List<CardInfo>();
+        PaymentAddress = paymentAddress.ThrowIfNull();
     }
 
     private Client() { }
 
-    public Name Name { get; private init; }
+    public Name Name { get; protected init; }
     public Rating Rating { get; set; }
-    public Address? PaymentAddress { get; private set; }
-    public IReadOnlyCollection<CardInfo> PaymentInfos => _paymentInfos;
+    public Address PaymentAddress { get; set; }
+    public IReadOnlyCollection<CardInfo> PaymentInfos => _paymentInfos.AsReadOnly();
 
     public void AddPaymentInfo(CardInfo cardInfo)
     {
+        cardInfo.ThrowIfNull();
+
         if (_paymentInfos.Contains(cardInfo))
             throw new OwnedCardInfoException(this, cardInfo);
 
@@ -35,6 +37,8 @@ public class Client : Entity<Client>
 
     public void RemovePaymentInfo(CardInfo cardInfo)
     {
+        cardInfo.ThrowIfNull();
+
         if (!_paymentInfos.Remove(cardInfo))
             throw new NonOwnedCardInfoException(this, cardInfo);
     }
