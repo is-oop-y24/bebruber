19edaac5fb71f8a5e7105dac025b44cc2510da1d
Bibletoon <alex8.ivanov@gmail.com>
2022-01-01 21:14:01 +0300

    Feat: add MudBlazor

--- Bebruber.Endpoints.UserWebClient/Program.cs
+++ Bebruber.Endpoints.UserWebClient/Program.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using MudBlazor.Services;
 
 namespace Bebruber.Endpoints.UserWebClient
 {
@@ -18,7 +19,7 @@ namespace Bebruber.Endpoints.UserWebClient
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+            builder.Services.AddMudServices();
             await builder.Build().RunAsync();
         }
     }
