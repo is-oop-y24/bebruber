9554c5fa09b948cf7fc2a964a960fd4c7f35272d
Bibletoon <alex8.ivanov@gmail.com>
2022-01-01 20:34:15 +0300

    Feat: add SignalR and core module

--- Bebruber.Endpoints.Server/Startup.cs
+++ Bebruber.Endpoints.Server/Startup.cs
@@ -1,16 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Bebruber.Core;
 
 namespace Bebruber.Endpoints.Server
 {
@@ -28,10 +22,12 @@ namespace Bebruber.Endpoints.Server
         {
 
             services.AddControllers();
+            services.AddSignalR();
+            services.AddCoreModule();
             services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Bebruber.Endpoints.Server", Version = "v1" });
-            });
+                                   {
+                                       c.SwaggerDoc("v1", new OpenApiInfo { Title = "Bebruber.Endpoints.Server", Version = "v1" });
+                                   });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
