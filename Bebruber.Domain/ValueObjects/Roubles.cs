f44a3fb339e8623713ded1a408d44e1f80d64185
ronimizy <71693183+ronimizy@users.noreply.github.com>
2022-01-03 19:02:56 +0300

    feat: roubles vo

---
+++ Bebruber.Domain/ValueObjects/Roubles.cs
@@ -0,0 +1,10 @@
+using Bebruber.Domain.Tools;
+using Bebruber.Domain.ValueObjects.Exceptions;
+
+namespace Bebruber.Domain.ValueObjects;
+
+public class Roubles : ValueOf<decimal, Roubles>
+{
+    protected Roubles(decimal value)
+        : base(value, d => d >= 0, new InvalidRoublesValueException(value)) { }
+}
