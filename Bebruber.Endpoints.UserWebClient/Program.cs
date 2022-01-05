e2335ec803fd0c44274b6d6456c788f6e4555f0e
Bibletoon <alex8.ivanov@gmail.com>
2022-01-05 16:14:35 +0300

    Feat: add blazorise

--- Bebruber.Endpoints.UserWebClient/Program.cs
+++ Bebruber.Endpoints.UserWebClient/Program.cs
@@ -1,14 +1,12 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
 using FisSst.BlazorMaps.DependencyInjection;
-using MudBlazor.Services;
 
 namespace Bebruber.Endpoints.UserWebClient
 {
@@ -20,7 +18,12 @@ namespace Bebruber.Endpoints.UserWebClient
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Services.AddMudServices();
+            builder.Services.AddBlazorise( options =>
+                                           {
+                                               options.ChangeTextOnKeyPress = true;
+                                           } )
+                   .AddBootstrapProviders()
+                   .AddFontAwesomeIcons();
             builder.Services.AddBlazorLeafletMaps();
 
             await builder.Build().RunAsync();
