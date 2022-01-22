08746f433c5e682d18103c800cf9b57d5e5472a3
Serelllka <vprogamervip@gmail.com>
2022-01-22 17:51:30 +0300

    feat: adds new commands

---
+++ src/Bebruber.Application/Rides/Commands/DriverArrivedCommandHandler.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Bebruber.DataAccess;
+using Bebruber.Domain.Entities;
+using Bebruber.Domain.Services;
+using Bebruber.Domain.ValueObjects.Ride;
+using Bebruber.Utility.Extensions;
+using MediatR;
+
+namespace Bebruber.Application.Rides.Commands;
+
+public class DriverArrivedCommandHandler
+{
+    public record Command(Guid RideId, Guid ClientId) : IRequest<Response>;
+    
+    public record Response(bool Status);
+
+    public class CommandHandler : IRequestHandler<Command, Response>
+    {
+        private readonly IRideService _rideService;
+        private readonly BebruberDatabaseContext _databaseContext;
+        private readonly IClientNotificationService _clientNotificationService;
+        
+        public CommandHandler(
+            IRideService rideService,
+            BebruberDatabaseContext databaseContext,
+            IClientNotificationService clientNotificationService)
+        {
+            _rideService = rideService;
+            _databaseContext = databaseContext;
+            _clientNotificationService = clientNotificationService;
+        }
+        
+        public async Task<Response> Handle(Command request, CancellationToken cancellationToken)
+        {
+            Ride? ride = await _databaseContext.Rides.FindAsync(request.RideId);
+            Client? client = await _databaseContext.Clients.FindAsync(request.ClientId);
+            ride = ride.ThrowIfNull();
+            client = client.ThrowIfNull();
+            await _rideService.SetRideDriverArrivedAsync(ride, cancellationToken);
+            await _clientNotificationService.NotifyDriverArrivedAsync(client, cancellationToken);
+            return new Response(true);
+        }   
+    }
+}
--- src/Bebruber.Application/Rides/Commands/DriverLocationUpdateCommand.cs
+++ src/Bebruber.Application/Rides/Commands/DriverLocationUpdateCommand.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Bebruber.Common.Dto;
+using Bebruber.DataAccess;
 using Bebruber.Domain.Entities;
 using Bebruber.Domain.Services;
 using Bebruber.Domain.ValueObjects.Ride;
+using Bebruber.Utility.Extensions;
 using FluentResults;
 using MediatR;
 
@@ -11,26 +14,30 @@ namespace Bebruber.Application.Rides.Commands;
 
 public class DriverLocationUpdateCommand
 {
-    public record Command(
-        Guid DriverId,
-        Coordinate Coord) : IRequest<Response>;
+    public record Command(Guid DriverId, CoordinateDto Coord) : IRequest<Response>;
     
-    public record Response();
+    public record Response(bool Status);
 
     public class CommandHandler : IRequestHandler<Command, Response>
     {
         private readonly IDriverLocationService _driverLocationService;
-        
-        CommandHandler(
+        private readonly BebruberDatabaseContext _databaseContext;
+
+        public CommandHandler(
             IDriverLocationService driverLocationService,
-            )
+            BebruberDatabaseContext databaseContext)
         {
             _driverLocationService = driverLocationService;
+            _databaseContext = databaseContext;
         }
         
         public async Task<Response> Handle(Command request, CancellationToken cancellationToken)
         {
-            await _driverLocationService.UpdateDriverLocationAsync()
+            Driver? driver = await _databaseContext.Drivers.FindAsync(request.DriverId);
+            driver = driver.ThrowIfNull();
+            var coordinate = new Coordinate(request.Coord.Latitude, request.Coord.Longitude);
+            await _driverLocationService.UpdateDriverLocationAsync(driver, coordinate, cancellationToken);
+            return new Response(true);
         }   
     }
 }
