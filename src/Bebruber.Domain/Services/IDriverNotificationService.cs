11b36cbedb94d1f4b39edfa9ebfdda53e9fbd40d
ronimizy <71693183+ronimizy@users.noreply.github.com>
2022-01-22 15:19:28 +0300

    refactor: IDriverNotificationService.NotifySuccessfulAcceptanceAsync

--- src/Bebruber.Domain/Services/IDriverNotificationService.cs
+++ src/Bebruber.Domain/Services/IDriverNotificationService.cs
@@ -8,6 +8,6 @@ namespace Bebruber.Domain.Services;
 public interface IDriverNotificationService
 {
     Task OfferRideToDriverAsync(Driver driver, RideEntry rideEntry, TimeSpan awaitingTimeSpan, CancellationToken cancellationToken);
-    Task NotifySuccessfulAcceptanceAsync(Driver driver, RideEntry rideEntry, CancellationToken cancellationToken);
+    Task NotifySuccessfulAcceptanceAsync(Driver driver, Ride rideEntry, CancellationToken cancellationToken);
     Task NotifyFailedAcceptanceAsync(Driver driver, CancellationToken cancellationToken);
 }
