6fbe8e2e99d06e7692619127ac321b5f78ea51fc
Serelllka <vprogamervip@gmail.com>
2022-01-22 17:12:06 +0300

    refactor: drive accept command

---
+++ src/Bebruber.Application/Rides/Commands/DriverLocationUpdateCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Bebruber.Domain.Entities;
+using Bebruber.Domain.Services;
+using Bebruber.Domain.ValueObjects.Ride;
+using FluentResults;
+using MediatR;
+
+namespace Bebruber.Application.Rides.Commands;
+
+public class DriverLocationUpdateCommand
+{
+    public record Command(
+        Guid DriverId,
+        Coordinate Coord) : IRequest<Response>;
+    
+    public record Response();
+
+    public class CommandHandler : IRequestHandler<Command, Response>
+    {
+        private readonly IDriverLocationService _driverLocationService;
+        
+        CommandHandler(
+            IDriverLocationService driverLocationService,
+            )
+        {
+            _driverLocationService = driverLocationService;
+        }
+        
+        public async Task<Response> Handle(Command request, CancellationToken cancellationToken)
+        {
+            await _driverLocationService.UpdateDriverLocationAsync()
+        }   
+    }
+}
