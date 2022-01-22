857701743f17a02469d0b177c2f35d240bccf696
Serelllka <vprogamervip@gmail.com>
2022-01-22 18:48:19 +0300

    feat: adds cancel search command

---
+++ src/Bebruber.Application/Rides/Commands/CancelSearchCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Bebruber.Domain.Services;
+using FluentValidation;
+using MediatR;
+
+namespace Bebruber.Application.Rides.Commands;
+
+public class CancelSearchCommand
+{
+    public record Command(Guid RideId) : IRequest<Response>;
+
+    public record Response(bool Status);
+
+    public class CommandHandler : IRequestHandler<Command, Response>
+    {
+        private readonly IRideQueueService _rideQueueService;
+
+        public CommandHandler(IRideQueueService rideQueueService)
+        {
+            _rideQueueService = rideQueueService;
+        }
+
+        public async Task<Response> Handle(Command request, CancellationToken cancellationToken)
+        {
+            await _rideQueueService.DequeueRideEntryAsync(request.RideId, cancellationToken);
+            return new Response(true);
+        }
+    }
+}
