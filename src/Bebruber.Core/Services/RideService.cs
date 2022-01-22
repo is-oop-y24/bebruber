4ae5889a7e5a987825a252a7afaee41efaab7353
ronimizy <71693183+ronimizy@users.noreply.github.com>
2022-01-22 16:30:08 +0300

    refactor: moved ride service to .Core

--- src/Bebruber.Core/Services/RideService.cs
+++ src/Bebruber.Core/Services/RideService.cs
@@ -1,24 +1,17 @@
-using System;
-using System.Threading;
-using System.Threading.Tasks;
-using Bebruber.DataAccess;
 using Bebruber.Domain.Entities;
 using Bebruber.Domain.Models;
 using Bebruber.Domain.Services;
 using Bebruber.Domain.ValueObjects.Ride;
-using Bebruber.Utility.Extensions;
 
-namespace Bebruber.Application.Services;
+namespace Bebruber.Core.Services;
 
 public class RideService : IRideService
 {
-    private readonly BebruberDatabaseContext _context;
     private readonly IPricingService _pricingService;
     private readonly ITimeProviderService _timeProviderService;
     private readonly IClientNotificationService _clientNotificationService;
 
     public RideService(
-        BebruberDatabaseContext context,
         IPricingService pricingService,
         ITimeProviderService timeProviderService,
         IClientNotificationService clientNotificationService)
@@ -26,10 +19,9 @@ public class RideService : IRideService
         _pricingService = pricingService;
         _timeProviderService = timeProviderService;
         _clientNotificationService = clientNotificationService;
-        _context = context.ThrowIfNull();
     }
 
-    public async Task<Ride> RegisterRideAsync(RideContext context, CancellationToken cancellationToken)
+    public Task<Ride> RegisterRideAsync(RideContext context, CancellationToken cancellationToken)
     {
         Roubles cost = _pricingService.Calculate(context);
         DateTime currentDateTime = _timeProviderService.GetCurrentDateTime();
@@ -45,38 +37,24 @@ public class RideService : IRideService
         context.Client.AddRide(ride);
         context.Driver.AddRide(ride);
 
-        _context.Clients.Update(context.Client);
-        _context.Drivers.Update(context.Driver);
-        await _context.Rides.AddAsync(ride, cancellationToken);
-        await _context.SaveChangesAsync(cancellationToken);
-
-        return ride;
+        return Task.FromResult(ride);
     }
 
     public async Task SetRideDriverArrivedAsync(Ride ride, CancellationToken cancellationToken)
     {
         await _clientNotificationService.NotifyDriverArrivedAsync(ride.Client, cancellationToken);
         ride.State = RideState.DriverArrived;
-
-        _context.Rides.Update(ride);
-        await _context.SaveChangesAsync(cancellationToken);
     }
 
     public async Task StartRideAsync(Ride ride, CancellationToken cancellationToken)
     {
         await _clientNotificationService.NotifyRideStartedAsync(ride.Client, cancellationToken);
         ride.State = RideState.Started;
-
-        _context.Rides.Update(ride);
-        await _context.SaveChangesAsync(cancellationToken);
     }
 
     public async Task FinishRideAsync(Ride ride, CancellationToken cancellationToken)
     {
         await _clientNotificationService.NotifyRideFinishedAsync(ride.Client, cancellationToken);
         ride.State = RideState.Finished;
-
-        _context.Rides.Update(ride);
-        await _context.SaveChangesAsync(cancellationToken);
     }
 }
