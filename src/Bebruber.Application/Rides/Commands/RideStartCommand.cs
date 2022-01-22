f0890737b05f61c2edd3058c41f13aa82dc4a235
Serelllka <vprogamervip@gmail.com>
2022-01-22 16:09:40 +0300

    feat: adds ride start command

---
+++ src/Bebruber.Application/Rides/Commands/RideStartCommand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Bebruber.DataAccess;
+using Bebruber.Domain.Entities;
+using Bebruber.Domain.Services;
+using MediatR;
+
+namespace Bebruber.Application.Rides.Commands;
+
+public class RideStartCommand
+{
+    public record Command(
+        Guid RideId) : IRequest<Response>;
+    
+    public record Response(
+        bool Status);
+
+    public class CommandHandler : IRequestHandler<Command, Response>
+    {
+        private readonly IRideService _rideService;
+        private readonly BebruberDatabaseContext _databaseContext;
+
+        CommandHandler(
+            IRideService rideService,
+            BebruberDatabaseContext databaseContext)
+        {
+            _rideService = rideService;
+            _databaseContext = databaseContext;
+        }
+        
+        public async Task<Response> Handle(Command request, CancellationToken cancellationToken)
+        {
+            Ride? ride = await _databaseContext.Rides.FindAsync(request.RideId);
+            if (ride is not null)
+            {
+                await _rideService.FinishRideAsync(ride, cancellationToken);   
+            }
+
+            return new Response(ride is not null);
+        }   
+    }
+}
