ca1cf3f1bc6ccda21dc0f8114d56468620560334
ronimizy <71693183+ronimizy@users.noreply.github.com>
2022-01-23 16:59:12 +0300

    chore: default implementation for TimeProviderService

--- src/Application/Bebruber.Application.Services/TimeProviderService.cs
+++ src/Application/Bebruber.Application.Services/TimeProviderService.cs
@@ -5,7 +5,5 @@ namespace Bebruber.Application.Services;
 public class TimeProviderService : ITimeProviderService
 {
     public DateTime GetCurrentDateTime()
-    {
-        throw new NotImplementedException();
-    }
+        => DateTime.UtcNow;
 }
