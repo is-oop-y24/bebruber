87ab2455e8fd213b033de8d758bd000968ec9d75
Bibletoon <alex8.ivanov@gmail.com>
2022-01-23 12:35:27 +0300

    Feat: add password VO

---
+++ src/Bebruber.Domain/ValueObjects/User/PasswordHash.cs
@@ -0,0 +1,10 @@
+﻿using Bebruber.Domain.Tools;
+
+namespace Bebruber.Domain.ValueObjects.User;
+
+public class PasswordHash : ValueOf<string, PasswordHash>
+{
+    public PasswordHash(string value)
+        : base(value)
+    { }
+}
