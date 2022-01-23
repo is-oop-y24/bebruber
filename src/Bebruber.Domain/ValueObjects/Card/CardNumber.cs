efaf78b8f4bf23e0b6b74a5281e20eb90ce23c86
Bill Williams <eid20021@gmail.com>
2022-01-23 15:48:02 +0300

    chore: card tostring stringbuider

--- src/Bebruber.Domain/ValueObjects/Card/CardNumber.cs
+++ src/Bebruber.Domain/ValueObjects/Card/CardNumber.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading.Channels;
 using Bebruber.Domain.Tools;
 using Bebruber.Domain.ValueObjects.Exceptions;
 
@@ -12,5 +15,9 @@ public class CardNumber : ValueOf<string, CardNumber>
     public static Regex Regex { get; } = new Regex("[0-9]{16}", RegexOptions.Compiled);
 
     public override string ToString()
-        => $"{Value.Replace(Value.Substring(4, 8), "********")}";
+    {
+        ReadOnlySpan<char> startSpan = Value.AsSpan().Slice(start: 0, length: 4);
+        ReadOnlySpan<char> endSpan = Value.AsSpan().Slice(start: 12, length: 4);
+        return new StringBuilder(16).Append(startSpan).Insert(4, "*", 8).Append(endSpan).ToString();
+    }
 }
