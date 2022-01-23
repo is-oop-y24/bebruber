3252f94b98f7c3ac5d72eb3077314206b1655a5c
Serelllka <vprogamervip@gmail.com>
2022-01-24 00:06:08 +0300

    feat: adds driver controller

---
+++ src/Endpoints/Bebruber.Endpoints.Server/Controllers/DriverController.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Bebruber.Application.Requests.Rides.Commands;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bebruber.Endpoints.Server.Controllers;
+
+[ApiController]
+[Route("api/rides")]
+public class DriverController : ControllerBase
+{
+    private readonly IMediator _mediator;
+    
+    public DriverController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    public async Task<ActionResult<AcceptRide.Response>> AcceptRide(AcceptRide.Command command)
+    {
+        return await _mediator.Send(command);
+    }
+}
