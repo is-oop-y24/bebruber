15860e8bcb87096523f2d5189726551b9d6c9acf
Serelllka <vprogamervip@gmail.com>
2022-01-24 02:19:48 +0300

    feat: controller

--- src/Endpoints/Bebruber.Endpoints.Server/Controllers/ClientController.cs
+++ src/Endpoints/Bebruber.Endpoints.Server/Controllers/ClientController.cs
@@ -1,5 +1,22 @@
-﻿namespace Bebruber.Endpoints.Server.Controllers;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Bebruber.Domain.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
-public class ClientController
+namespace Bebruber.Endpoints.Server.Controllers;
+
+public class ClientController : ControllerBase
 {
+    [HttpGet("get-payment-methods")]
+    [Authorize(AuthenticationSchemes = "Bearer", Roles="Client")]
+    public ActionResult<List<string>> GetPaymentMethods()
+    {
+        var client = (Client)User.Identity;
+        var infos = client.PaymentInfos.Select(item => item.ToString()).ToList();
+        return infos;
+    }
 }
