1a3b739dd50ed28057174c39eb457cef5bad3d25
Serelllka <vprogamervip@gmail.com>
2022-01-24 00:48:31 +0300

    feat: implements controller methods

--- src/Endpoints/Bebruber.Endpoints.Server/Controllers/DriverController.cs
+++ src/Endpoints/Bebruber.Endpoints.Server/Controllers/DriverController.cs
@@ -2,6 +2,7 @@
 using Bebruber.Application.Requests.Rides.Commands;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
 
 namespace Bebruber.Endpoints.Server.Controllers;
 
@@ -24,6 +25,30 @@ public class DriverController : ControllerBase
 
     [HttpPost("create")]
     public async Task<ActionResult<CreateRide.Response>> CreateRide(CreateRide.Command command)
+    {
+        return await _mediator.Send(command);
+    }
+
+    [HttpPost("startRide")]
+    public async Task<ActionResult<StartRide.Response>> StartRide(StartRide.Command command)
+    {
+        return await _mediator.Send(command);
+    }
+    
+    [HttpPost("finishRide")]
+    public async Task<ActionResult<FinishRide.Response>> StartRide(FinishRide.Command command)
+    {
+        return await _mediator.Send(command);
+    }
+
+    [HttpPatch("arrived")]
+    public async Task<ActionResult<DriverArrived.Response>> Arrived(DriverArrived.Command command)
+    {
+        return await _mediator.Send(command);
+    }
+    
+    [HttpPut("cancel")]
+    public async Task<ActionResult<CancelSearch.Response>> Arrived(CancelSearch.Command command)
     {
         return await _mediator.Send(command);
     }
