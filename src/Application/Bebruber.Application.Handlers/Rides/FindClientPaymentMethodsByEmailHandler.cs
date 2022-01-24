418f4cd76428417d6f3bdb978a7997b7d84bfb5e
Serelllka <vprogamervip@gmail.com>
2022-01-24 04:11:55 +0300

    feat: add find client payment method

---
+++ src/Application/Bebruber.Application.Handlers/Rides/FindClientPaymentMethodsByEmailHandler.cs
@@ -0,0 +1,36 @@
+﻿using Bebruber.DataAccess;
+using Bebruber.Domain.Entities;
+using Bebruber.Domain.Services;
+using Bebruber.Identity;
+using Bebruber.Utility.Extensions;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Command = Bebruber.Application.Requests.Accounts.Queries.FindClientPaymentMethodsByEmail.Command;
+using Response = Bebruber.Application.Requests.Accounts.Queries.FindClientPaymentMethodsByEmail.Response;
+
+namespace Bebruber.Application.Handlers.Rides;
+
+public class FindClientPaymentMethodsByEmailHandler : IRequestHandler<Command, Response>
+{
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly BebruberDatabaseContext _context;
+
+    public FindClientPaymentMethodsByEmailHandler(
+        UserManager<ApplicationUser> userManager,
+        BebruberDatabaseContext context)
+    {
+        _userManager = userManager;
+        _context = context;
+    }
+
+    public async Task<Response> Handle(Command request, CancellationToken cancellationToken)
+    {
+        ApplicationUser? applicationUser = await _userManager.FindByEmailAsync(request.Email);
+        Client? user = await _context.FindAsync(
+            applicationUser.ModelType,
+            applicationUser.ModelId) as Client;
+        user = user.ThrowIfNull();
+        return new Response(
+            user.PaymentInfos.Select(i => i.ToString()).ToList());
+    }
+}
--- src/Endpoints/Bebruber.Endpoints.Server/Controllers/ClientController.cs
+++ src/Endpoints/Bebruber.Endpoints.Server/Controllers/ClientController.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using Bebruber.Application.Requests.Accounts;
+using Bebruber.Application.Requests.Accounts.Queries;
 using Bebruber.Domain.Entities;
 using FluentResults;
+using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,15 +15,24 @@ namespace Bebruber.Endpoints.Server.Controllers;
 
 public class ClientController : ControllerBase
 {
+    private readonly IMediator _mediator;
+
+    public ClientController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
     [HttpGet("get-payment-methods")]
-    [Authorize(AuthenticationSchemes = "Bearer", Roles="Client")]
-    public ActionResult<List<string>> GetPaymentMethods()
+    [Authorize(AuthenticationSchemes = "Bearer")]
+    public async Task<ActionResult<FindClientPaymentMethodsByEmail.Response>> GetPaymentMethods()
     {
         var userIdentity = (ClaimsIdentity)User.Identity;
-        foreach (var item in userIdentity.Claims)
-        {
-        }
 
-        return new List<string>();
+        Claim claim = userIdentity.Claims
+            .FirstOrDefault(c => c.Type.Equals(ClaimTypes.NameIdentifier));
+        string email = claim.Value;
+
+        var command = new FindClientPaymentMethodsByEmail.Command(email);
+        return await _mediator.Send(command);
     }
 }
